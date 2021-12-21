53e23cf7aad2a06bf2a29d12a39e70da058d65bb
snoy <tr.bhunnathon_st@tni.ac.th>
2021-12-21 13:49:58 +0700

    edit data

--- src/MockDatabase.js
+++ src/MockDatabase.js
@@ -2,13 +2,11 @@ let dataBase = {
     baseColor:["color1", "color2"],
     customer:[
         {
-            fname:"customer1",
-            lname:"mock1",
+            name:"customer1",
             address:"11/11 road xxx city xxx country xxx"
         },
         {
-            fname:"customer2",
-            lname:"mock2",
+            name:"customer2",
             address:"11/11 road xxx city xxx country xxx"
         }
     ],
